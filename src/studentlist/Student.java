035fa1adb124fa2a6c930680c8ba1e17e5bf21a8
Alex <wilkal@sheridancollege.ca>
2020-06-06 13:21:05 -0400

    Added address attribute with getter and setter

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -8,6 +8,8 @@ package studentlist;
 public class Student {
     //test
     private String name;
+    private int id;
+    private String address;
 
     public Student(String name) {
         this.name = name;
@@ -19,6 +21,20 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
     }
     
 }
